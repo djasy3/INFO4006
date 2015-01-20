d9718200eb69601946da7522f72abccd741727c9
Arthson Mukeya <djasy3@gmail.com>
2015-01-20 00:44:04 -0400

    Update getReviews.php

--- getReviews.php
+++ getReviews.php
@@ -1,23 +1,24 @@
 <?php
 
-require_once("./admin/code/4006_fns.php");
-			$bdd = cnx_bdd('eam6014', 'bddparm');
+	require_once("./admin/code/4006_fns.php");
+
+	$bdd = cnx_bdd('eam6014', 'bddparm');
 	$fetchAllReviews = $bdd -> prepare('SELECT Score, Commentaire FROM Appreciation where IdItem = ?');
 	$fetchAllReviews -> execute(array($_GET['id']));
 
 	$fetchAllReviewsData = $fetchAllReviews -> fetch();
 
-		if($fetchAllReviews->rowCount() == 0) //aucune review
-			echo "</br>Aucune Appreciation, soyez le premier!";
-		else
+	if($fetchAllReviews->rowCount() == 0) //aucune review
+		echo "</br>Aucune Appreciation, soyez le premier!";
+	else
+	{
+		echo "<ul>";
+		do
 		{
-			echo "<ul>";
-			do
-			{
-				//On crée une liste des reviews
-				echo "Score: " . $fetchAllReviewsData['Score'] . "/5</br>
-				Commentaire: " . $fetchAllReviewsData['Commentaire'] . "</br></br>";
-			}while($fetchAllReviewsData = $fetchAllReviews -> fetch());
-			echo "</ul>";
-		}
+			//On crée une liste des reviews
+			echo "Score: " . $fetchAllReviewsData['Score'] . "/5</br>
+			Commentaire: " . $fetchAllReviewsData['Commentaire'] . "</br></br>";
+		}while($fetchAllReviewsData = $fetchAllReviews -> fetch());
+		echo "</ul>";
+	}
 ?>
